1afe6e98db35bfff4cd3d2d8cb401195bd6d2be9
NDRAEY <pikachu_andrey@vk.com>
2026-08-03 14:09:44 +0900

    Allow reading from stdin if `-` is provided

--- geko/cli/src/io.rs
+++ geko/cli/src/io.rs
@@ -2,7 +2,7 @@
 use camino::Utf8PathBuf;
 use common::{
     bail,
-    io::{IO, IOError},
+    io::{IOError, IO},
 };
 use std::{
     env, fs,
@@ -26,6 +26,11 @@ impl IO for CliIO {
 
     /// Canonicalize implementation
     fn canonicalize(&self, path: &Utf8PathBuf) -> Option<Utf8PathBuf> {
+        // If user wants to read from stdin (by providing "-"), keep it as-is.
+        if path == "-" {
+            return Some(path.clone());
+        }
+
         // Reading file
         match path.canonicalize_utf8() {
             Ok(path) => Some(path),
@@ -35,8 +40,14 @@ impl IO for CliIO {
 
     /// Read implementation
     fn read(&self, path: &Utf8PathBuf) -> String {
-        // Reading file
-        match fs::read_to_string(path) {
+        // Read from stdin if path is "-" (which means stdin), read a file otherwise.
+        let result = if path == "-" {
+            io::read_to_string(io::stdin())
+        } else {
+            fs::read_to_string(path)
+        };
+
+        match result {
             Ok(text) => text,
             Err(_) => bail!(IOError::FileNotFound(path.clone())),
         }
